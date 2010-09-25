de8d4eabc0f6ca510df8150d73b32eea0bef969e
Michael Davidsaver <mdavidsaver@bnl.gov>
2010-09-25 04:47:39 -0400

    update repeater endpoint

--- cac/repclient.py
+++ cac/repclient.py
@@ -3,54 +3,109 @@
 import socket, logging
 from socket import inet_aton
 
-log=logging.getLogger('cac.endpoint')
+log=logging.getLogger('cac.repclient')
 
 from util.ca import CAmessage
 from util.defs import CLIENT_PORT
 
 from twisted.internet import reactor
+from twisted.internet.task import LoopingCall
 from twisted.internet.protocol import Protocol, DatagramProtocol
 from twisted.internet.address import IPv4Address
+from twisted.internet.error import CannotListenError
+
+__all__=['client']
 
 class RepClient(DatagramProtocol):
     
-    def __init__(self, handler):
-        self.handler=handler
-
-    def doStart(self):
-        host=self.transport.getHost()
-        addr, self.port = host.host, host.port
+    def __init__(self, port=CLIENT_PORT):
+        self.handlers=set()
+        self.port=port
+        self.waitack=None
         
-        self.rep=IPv4Address('UDP',addr,CLIENT_PORT)
+        self.rep=('127.0.0.1',port)
 
-        reg=CAmessage(cmd=24, p2=inet_aton(addr))
-        self.sendto(reg.pack())
+    def stopProtocol(self):
+        if self.waitack:
+            self.waitack.cancel()
+            self.waitack=None
+
+    def sendReg(self):
+        if self.waitack:
+            self.waitack.cancel()
+            self.waitack=None
+
+        self.sendto('')
         
         self.waitack=reactor.callLater(2.0, self.noack)
 
     def ackreg(self, pkt):
-        self.waitack.cancel()
+        if self.waitack:
+            self.waitack.cancel()
+            self.waitack=None
 
     def noack(self):
         log.error("Failed to connect to CA Repeater")
+        self.sendto('')
+        self.waitack=reactor.callLater(4.0, self.noack)
 
     def sendto(self, msg):
         return self.transport.write(msg, self.rep)
 
     def datagramReceived(self, msg, peer):
-        if peer != self.rep:
-            log.warning('dropping UDP traffic from %s',peer)
-            return
         
         while msg is not None and len(msg)>=16:
         
             pkt, msg = CAmessage.unpack(msg)
         
             if pkt.cmd!=17:
-                self.handler(pkt, self)
+                for h in self.handlers:
+                    h(pkt, peer)
             else:
                 self.ackreg(pkt)
 
         if len(msg)>0:
             log.warning('dropping incomplete message')
 
+class RepManager(object):
+    
+    def __init__(self, port=CLIENT_PORT):
+        self._port=reactor.listenUDP(0, RepClient())
+        
+        self._watcher=LoopingCall(self.check)
+        self._watcher.start(30.0, now=False)
+        
+        self.check(first=True)
+        
+        reactor.addSystemEventTrigger("before", "shutdown", self.close)
+
+    def check(self,first=False):
+        try:
+            import subprocess, os, sys, time
+
+            R=reactor.listenUDP(self._port.protocol.port, DatagramProtocol())
+            R.stopListening()
+
+            log.info('Starting repeater')
+            subprocess.Popen([sys.executable, "-m", "cac.repeater"],
+                             close_fds=True)
+            
+            reactor.callLater(0.5, self._port.protocol.sendReg)
+        except CannotListenError:
+            if first:
+                self._port.protocol.sendReg()
+
+    def close(self):
+        log.debug('Repeater client stopping')
+        self._watcher.stop()
+        self._port.stopListening()
+
+    def add(self, handler):
+        self._port.protocol.handlers.add(handler)
+
+    def remove(self, handler):
+        if handler not in self._port.protocol.handlers:
+            return
+        self._port.protocol.handlers.remove(handler)
+
+client=RepManager()
