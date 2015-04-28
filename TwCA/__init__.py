2da803af23773728ab471d69416642450093d13d
Michael Davidsaver <mdavidsaver@bnl.gov>
2015-04-28 19:59:08 -0400

    python 2.7

--- TwCA/__init__.py
+++ TwCA/__init__.py
@@ -8,6 +8,8 @@ if not hasattr(logging, 'NullHandler'):
     class NullHandler(logging.Handler):
         def emit(self, record):
             pass
+else:
+    NullHandler = logging.NullHandler
 
 __h=NullHandler()
 logging.getLogger("TwCA").addHandler(__h)
